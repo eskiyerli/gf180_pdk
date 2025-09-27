82e539d1e260170ca2a3a93fdefedecb966dea4a
Murat Eskiyerli <eskiyerli@reveda.eu>
2025-09-27 20:46:47 +0200

    Addition of layoutLayers
    Add new symbol JSON files for various components in gf180_mcu
    
    Update layoutLayers.py file and stipples.
    
    Refactor layer definitions and update grid parameters
    
    - Renamed marker layer variables in layoutLayers.py for consistency and clarity.
    - Adjusted grid parameters in process.py to improve snap and major grid settings.
    
    Update symbol.json files to standardize attribute definitions and improve layout parameters

---
+++ pcells.py
@@ -0,0 +1,7 @@
+
+import revedaEditor.backend.dataDefinitions as ddef
+import revedaEditor.common.layoutShapes as lshp
+from revedaEditor.backend.pdkPaths import importPDKModule
+
+class baseCell(lshp.layoutPcell):
+    pass
---
+++ process.py
@@ -0,0 +1,34 @@
+#    “Commons Clause” License Condition v1.0
+#   #
+#    The Software is provided to you by the Licensor under the License, as defined
+#    below, subject to the following condition.
+#
+#    Without limiting other conditions in the License, the grant of rights under the
+#    License will not include, and the License does not grant to you, the right to
+#    Sell the Software.
+#
+#    For purposes of the foregoing, “Sell” means practicing any or all of the rights
+#    granted to you under the License to provide to third parties, for a fee or other
+#    consideration (including without limitation fees for hosting) a product or service whose value
+#    derives, entirely or substantially, from the functionality of the Software. Any
+#    license notice or attribution required by the License must also include this
+#    Commons Clause License Condition notice.
+#
+#   Add-ons and extensions developed for this software may be distributed
+#   under their own separate licenses.
+#
+#    Software: Revolution EDA
+#    License: Mozilla Public License 2.0
+#    Licensor: Revolution Semiconductor (Registered in the Netherlands)
+#
+# import revedaEditor.backend.dataDefinitions as ddef
+from revedaEditor.backend.pdkPaths import importPDKModule
+
+laylyr = importPDKModule('layoutLayers')
+
+# common process parameters
+dbu = 100  # grid points per micron
+snapGrid = 50 # 0.5
+majorGrid = 100 # 1 micron
+gdsUnit = "1 um"
+gdsPrecision = "1 nm"
